eb15e898fde10c63c429a88e92a9042825ee738b
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-01 01:01:31 +0100

    about section

--- csgcommunity-ui/src/app/app-routing.module.ts
+++ csgcommunity-ui/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/404.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'faq', component: FaqComponent, data: { title: 'FAQ' } },
+  { path: 'about', component: AboutComponent, data: { title: 'About Us'} },
   { path: '', component: HomeComponent, data: { title: 'Home' } },
   { path: '**', component: NotfoundComponent, data: { title: 'Not Found' } }
 ];
--- csgcommunity-ui/src/app/app.module.ts
+++ csgcommunity-ui/src/app/app.module.ts
@@ -13,24 +13,28 @@ import { NotfoundComponent } from './notfound/404.component';
 // services
 import { FaqService } from './faq/faq.service';
 
-// material icons
+// material
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaqComponent,
     HomeComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    AboutComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
+    // routing
     AppRoutingModule,
-    MatIconModule,
+    // material
     BrowserAnimationsModule,
+    MatIconModule,
     MatButtonModule,
     MatToolbarModule,
   ],
