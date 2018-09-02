a9ab6b177ee1aac73ae2cb365ba0bf5ca3d3dee3
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-02 12:15:07 +0100

    cleanup

--- csgcommunity-ui/src/environments/environment.prod.ts
+++ csgcommunity-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUri: 'http://localhost:4200/'
+  baseUri: 'http://localhost:4200/api/'
 };
--- csgcommunity-ui/src/environments/environment.ts
+++ csgcommunity-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUri: 'http://localhost:4200/'
+  baseUri: 'http://localhost:4200/api/'
 };
