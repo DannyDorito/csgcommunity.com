696a048e51bdebd84dafc5d8ba1bd103298ec015
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-17 21:31:28 +0100

    forums production if

--- csgcommunity-ui/src/app/forums/forums.component.ts
+++ csgcommunity-ui/src/app/forums/forums.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-forums',
   templateUrl: './forums.component.html',
   styleUrls: ['../app.component.css']
 })
-export class ForumsComponent { }
+export class ForumsComponent {
+  development = environment.production;
+}
