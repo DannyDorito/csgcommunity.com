32507f48328add79aefe6f1745287898b4803016
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-06-04 05:21:35 +0100

    removed alternate logos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', '../app.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  logoSource = '';
-
-  ngOnInit() {
-    const randomNum = this.getRandomInt(10);
-    if (randomNum <= 1) {
-      this.logoSource = 'assets/logo-large-cph.png';
-    } else if (randomNum >= 9) {
-      this.logoSource = 'assets/logo-large-dog.png';
-    } else {
-      this.logoSource = 'assets/logo-large.png';
-    }
-  }
-
-  getRandomInt(max: number) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+export class HomeComponent {
+  // getRandomInt(max: number) {
+  //   return Math.floor(Math.random() * Math.floor(max));
+  // }
 }
