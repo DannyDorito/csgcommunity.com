f8703d14b619fb4bbc334faef49f31133844e066
John Allison <john@jallison.co.uk>
2024-05-26 17:37:11 +0100

    lazy load server pics

--- src/app/servers/servers.module.ts
+++ src/app/servers/servers.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from "@angular/core";
 import { ServersComponent } from "./servers.component";
 import { ServersRoutingModule } from "./servers-routing.module";
 import { MatListModule } from "@angular/material/list";
+import { NgOptimizedImage } from '@angular/common'
 
 @NgModule({
     imports: [
         CommonModule,
         ServersRoutingModule,
         MatListModule,
+        NgOptimizedImage,
         ServersComponent,
     ]
 })
